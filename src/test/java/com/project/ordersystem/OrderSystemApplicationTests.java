ac323680ebd8c30c8e204e1ddabfca04f791f365
tunc dogan <tunc.dogan@n11.com>
2020-08-31 21:56:08 +0300

    Project Initialize

---
+++ src/test/java/com/project/ordersystem/OrderSystemApplicationTests.java
@@ -0,0 +1,12 @@
+package com.project.ordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
